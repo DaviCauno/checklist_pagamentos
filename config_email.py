74e48a9efc008b95aad29d84e71160b65a84b88a
Davi Cauno <79549708+DaviCauno@users.noreply.github.com>
2025-04-24 00:52:12 -0300

    Update config_email.py

--- config_email.py
+++ config_email.py
@@ -1,3 +1,3 @@
 EMAIL_REMETENTE = "davicaunosantos@yahoo.com.br"
 SENHA_APP = "1234"
-EMAIL_DESTINOS = ["davicaunosantos@yahoo.com.br"]
+EMAIL_DESTINOS = ["davicaunosantos@yahoo.com.br", "aline.mluiz1@gmail.com"]
