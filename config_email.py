122ea0dc2c349ed2d4eefc24f6a81f2ab24adeee
Davi Cauno <79549708+DaviCauno@users.noreply.github.com>
2025-04-24 00:47:25 -0300

    Add files via upload

---
+++ config_email.py
@@ -0,0 +1,4 @@
+EMAIL_REMETENTE = "davicaunosantos@yahoo.com.br"
+SENHA_APP = "1234"
+EMAIL_DESTINO = "davicaunosantos@yahoo.com.br"
+EMAIL_DESTINO = "aline.mluiz1@gmail.com"
---
+++ email_utils.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config_email import EMAIL_REMETENTE, SENHA_APP, EMAIL_DESTINO
+
+def enviar_email_novo_pagamento(ano, categoria, mes, valor, pago):
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL_REMETENTE
+    msg['To'] = EMAIL_DESTINO
+    msg['Subject'] = "📬 Novo pagamento adicionado"
+
+    corpo = f"""
+    Um novo pagamento foi adicionado:
+
+    🗓 Ano: {ano}
+    📂 Categoria: {categoria}
+    📅 Mês: {mes}
+    💰 Valor: R$ {valor:.2f}
+    ✅ Pago? {"Sim" if pago else "Não"}
+    """
+
+    msg.attach(MIMEText(corpo, 'plain'))
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as servidor:
+            servidor.starttls()
+            servidor.login(EMAIL_REMETENTE, SENHA_APP)
+            servidor.send_message(msg)
+            print("Email enviado com sucesso.")
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
